3e25ffb87fba3940c3e5ad76913843f0d3dad6df
Manuel <manuel.maria2003@hotmail.com>
2023-11-18 19:26:57 +0000

    Changed something

--- resources/views/pages/profile/cart.blade.php
+++ resources/views/pages/profile/cart.blade.php
@@ -1,28 +1,58 @@
+<!-- resources/views/home.blade.php -->
 @extends('layouts.app')
 
 @section('content')
-    <section class="small-container cart-page">
-      <table>
-        <tr>
-          <th>Product</th>
-          <th>Quantity</th>
-          <th>Subtotal</th>
-        </tr>
-        <tr>
-          <td>
-            <div class ="cart-info">
-              <img src="image">
-              <div>
-                <p> Blue Jeans</p>
-                <small>Price: 30.00€</small>
-                <br>
-                <a href = ""> Remove</a>
-              </div>
+    <section class="hero-section">
+        <div class="hero-image">
+            <!-- Vintage image goes here -->
+            <img class="hero-banner" src="{{ asset('images/heroBanner.jpg') }}" alt="Hero Banner">
+            
+            <!-- Semi-transparent title -->
+            <div class="image-overlay">
+                <h1>Antiquus</h1>
+                <h2>Shop Oldschool</h2>
             </div>
-          </td>
-          <td><input type= "number" value="1"</td>
-          <td>30.00€</td>
-        </tr>
-      </table>
+
+            <div class="image-overlay-btn">
+                <a href="">Shop Now</a>
+            </div>
+        </div>
+    </section>
+
+    <section class="product-section">
+        <h3>Some of our products</h2>
+
+        <div class="product-container">
+
+            <button class="prev-arrow" onclick="">&#8249;</button>
+
+            <!-- Product 1 -->
+            <div class="product">
+                <img src="{{ asset('images/default-product-image.png') }}" alt="Product 1">
+                <h4>Product 1</h4>
+                <p>Description of Product 1.</p>
+                <span>$19.99</span>
+            </div>
+
+            <!-- Product 2 -->
+            <div class="product">
+                <img src="{{ asset('images/default-product-image.png') }}" alt="Product 2">
+                <h4>Product 2</h4>
+                <p>Description of Product 2.</p>
+                <span>$24.99</span>
+            </div>
+
+            <!-- Product 3 -->
+            <div class="product">
+                <img src="{{ asset('images/default-product-image.png') }}" alt="Product 3">
+                <h4>Product 3</h4>
+                <p>Description of Product 3.</p>
+                <span>$29.99</span>
+            </div>
+
+            <button class="next-arrow" onclick="">&#8250;</button>
+
+        </div>
     </section>
 @endsection
+

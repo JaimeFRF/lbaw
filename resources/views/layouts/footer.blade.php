ab4a0a4c5cb82516f02f68410814f3eb75198294
up202108885 <up202108885@fe.up.pt>
2023-11-18 17:12:01 +0000

    correct footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="d-flex justify-content-between align-items-center bg-light mt-5 p-4">
-    <span>&copy; 2021 Your Website</span>
+    <span>&copy; 2023 Antiquus</span>
 
     <div class="footer-links">
         <a  class = "m-3" href="#" class="footer-link">FAQ</a>

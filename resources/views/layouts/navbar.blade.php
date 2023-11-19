49d8a19f3299048c1635c6fc7d36c9b763604ad4
Manuel <manuel.maria2003@hotmail.com>
2023-11-19 13:15:13 +0000

    Changed navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,17 +1,17 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
   <div class="container-fluid jusityf-content-between">
-    <a class="navbar-brand" href="{{route('home')}}"> <span>Antiquus</span> </a>
+    <a class="navbar-brand" href="{{route('home')}}"> <span class="fs-2 ms-4">Antiquus</span> </a>
 
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse align-items-center" id="navbarSupportedContent">
-      <!-- Categories Dropdown -->
-      <ul class="navbar-nav me-auto mb-lg-0 align-items-center w-75">
+
+    <div class="collapse navbar-collapse  " id="navbarSupportedContent">
+
+      <ul class="navbar-nav ms-auto mb-lg-0 align-items-center w-40  me-4">
         <li>
           <div class="dropdown m-2">
-      
-            <button class="btn btn-secondary dropdown-toggle" id = "categoriesDropdown" data-toggle="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" id="categoriesDropdown" data-toggle="dropdown">
               Categories
             </button>
             <nav class="dropdown-menu">
@@ -23,53 +23,50 @@
             </nav>
           </div>
         </li>
-        <li class = "w-100">
+        <li class="w-100">
           <!-- Search bar -->
-          <form class="d-flex" method = "POST" action = "#">
+          <form class="d-flex justify-content-end me-2" method="POST" action="#">
             @csrf
-            
-            <input class="form-control me-2" type="search" name = "search" placeholder="Search">
+            <input class="form-control form-control-sm" type="search" name="search" placeholder="Search">
           </form>
         </li>
-      </ul>  
-      
+      </ul>
+
       <!-- User features -->
-      <div class = "navbar-nav d-flex flex-row">
+      <div class="navbar-nav d-flex flex-row">
         @if (Auth::check())    
-            @if(!Auth::user()->isadmin)
-              <a title="Wishlist" class="btn btn-primary m-2" href = "">
-                <i class="fa fa-heart"></i>
-              </a>
-            @endif
-            
-     
-            @php
-              $n = DB::table('notification')->where('id_user', '=', Auth::id())->count();
-            @endphp
+          @if(!Auth::user()->isadmin)
+            <a title="Wishlist" class="m-3 me-4" href="">
+              <i class="fa fa-heart text-white fs-5"></i>
+            </a>     
+          @endif
 
-            <a title="Notifications" class="btn btn-primary m-2" href = "">   
-              @if($n > 0){{$n}}@endif  
-                <i class="fa fa-bell"></i>
+          @php
+            $n = DB::table('notification')->where('id_user', '=', Auth::id())->count();
+          @endphp
+
+          <a title="Notifications" class="m-3 me-4" href="">   
+            @if($n > 0){{$n}}@endif  
+            <i class="fa fa-bell text-white fs-5"></i>
+          </a> 
+          
+          @if(!Auth::user()->isadmin)
+            <a title="Cart" class="m-3 me-4" href="{{route('cart')}}">
+              <i class="fa fa-shopping-cart text-white fs-5"></i>
+              <span class="text-white">(0)</span>
             </a> 
-            
-            @if(!Auth::user()->isadmin)
-              <a title="Cart" class="btn btn-primary m-2"  href = "{{route('cart')}}">
-                <i class="fa fa-shopping-cart"></i>
-              </a> 
-            @endif
+          @endif
 
-            <a title="Profile" class="btn btn-primary m-2" href = "{{route('profile')}}">
-              <i class="fa fa-user"></i>
-              <span>{{Auth::user()->username}}</span>
-            </a>
-            
+          <a title="Profile" class="m-3 me-4" href="{{route('profile')}}">
+            <i class="fa fa-user text-white fs-5"></i>
+          </a>
 
-            <a title="Logout" class="btn btn-primary m-2" href = "{{route('logout')}}">
-              <i class="fa fa-sign-out-alt"></i>
-            </a> 
+          <a title="Logout" class="m-3 me-4" href="{{route('logout')}}">
+            <i class="fa fa-sign-out-alt fs-5 text-white"></i>
+          </a> 
 
         @else
-          <a title="Login" class="btn btn-primary m-2" href = "{{route('login')}}"> 
+          <a title="Login" class="btn btn-primary m-3" href="{{route('login')}}"> 
             <i class="fa fa-sign-in-alt"></i>
             <span>Login</span>
           </a>

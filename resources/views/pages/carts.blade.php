ea088573d67893d82a70279fb0452c9a92d4335a
Manuel <manuel.maria2003@hotmail.com>
2023-11-20 15:35:38 +0000

    Cart buy

--- resources/views/pages/carts.blade.php
+++ resources/views/pages/carts.blade.php
@@ -11,19 +11,40 @@
           <th>Quantity</th>
           <th>Subtotal</th>
         </tr>
-    @foreach($items as $item)
-    @include('partials.cart', ['item' => $item])
-    @endforeach
-    <div class = "total-price">
+        @foreach($items as $item)
+        @include('partials.cart', ['item' => $item])
+        @endforeach
+    </table>
+
+    <div  class = "m-4 cart-total">
+        <h4 class="fw-bold">Cart Total</h4>
         <table>
-            <tr>
-                <td>Total</td>
-                <td>{{ $items->sum(function($item) { return $item->price * $item->pivot->quantity; }) }}€
-                </td>
-            </tr>
+        <tr>
+            <td> Cart Subtotal</td>
+            <td>{{ $items->sum(function($item) { return $item->price * $item->pivot->quantity; }) }}€</td>
+        </tr>
+        <tr>
+            <td>Shipping</td>
+            <td>Free</td>
+        </tr>
+        <tr>
+            <td class="fw-bold">Total</td>
+            <td class="fw-bold">{{ $items->sum(function($item) { return $item->price * $item->pivot->quantity; }) }}€</td>
+        </tr>
         </table>
+        <div class="cart-buttons d-flex justify-content-around">
+            <button class="btn btn-success m-2 w-100">
+                    Checkout
+                </button>
+            <button class="btn btn-outline-danger m-2 w-100">
+                    Empty Cart
+            </button>
+        </div>
+
     </div>
+
 </section>
+
 @endsection
 
 

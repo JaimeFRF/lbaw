169f088d9b60b727b2e94e90552e9541f49d6241
jaimefrf <jaimefrfonseca03@gmail.com>
2023-12-19 18:44:35 +0000

    bug fix v2

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -195,6 +195,7 @@ public function updateItem(Request $request, $id)
         return response()->json(['message' => 'Item not found, wrong id'], 404);
     }
 
+
     $request->validate([
         'name' => 'required|string|max:255',
         'price' => 'required|numeric',
@@ -208,48 +209,50 @@ public function updateItem(Request $request, $id)
     ]);
 
     $wasOutOfStock = $item->stock == 0;
-    $oldPrice = $item->price;
+    $oldPrice = $item->price; 
+
 
     // Update item attributes
     $item->fill($request->only([
         'name', 'price', 'stock', 'color', 'era', 'fabric', 'description', 'brand', 'subcategory'
     ]));
+    
 
     $item->stock = $request->stock;
     $isInStock = $item->stock > 0;
-    $changedPrice = $oldPrice != $request->price;
+
 
     // Specific attributes for different item types
-    switch ($request->category) {
+    switch ($request->categoryEdit) {
         case 'Shirt':
-            $item->shirt()->update([
-                'shirt_type' => $request->subCategoryEdit,
-                'size' => $request->size,
-            ]);
+            $shirt = Shirt::where('id_item', $item->id)->first();
+            $shirt->shirt_type = $request->subCategoryEdit;
+            $shirt->size = $request->size;
+            $shirt->save();
             break;
         case 'Tshirt':
-            $item->tshirt()->update([
-                'tshirt_type' => $request->subCategoryEdit,
-                'size' => $request->size,
-            ]);
+            $tshirt = Tshirt::where('id_item', $item->id)->first();
+            $tshirt->tshirt_type = $request->subCategoryEdit;
+            $tshirt->size = $request->size;
+            $tshirt->save();
             break;
         case 'Jacket':
-            $item->jacket()->update([
-                'jacket_type' => $request->subCategoryEdit,
-                'size' => $request->size,
-            ]);
+            $jacket = Jacket::where('id_item', $item->id)->first();
+            $jacket->jacket_type = $request->subCategoryEdit;
+            $jacket->size = $request->size;
+            $jacket->save();
             break;
         case 'Jeans':
-            $item->jeans()->update([
-                'jeans_type' => $request->subCategoryEdit,
-                'size' => $request->size,
-            ]);
+            $jeans = Jeans::where('id_item', $item->id)->first();
+            $jeans->jeans_type = $request->subCategoryEdit;
+            $jeans->size = $request->size;
+            $jeans->save();
             break;
         case 'Sneakers':
-            $item->sneakers()->update([
-                'sneakers_type' => $request->subCategoryEdit,
-                'size' => $request->size,
-            ]);
+            $sneakers = Sneakers::where('id_item', $item->id)->first();
+            $sneakers->sneakers_type = $request->subCategoryEdit;
+            $sneakers->size = $request->size;
+            $sneakers->save();
             break;
         default:
             break;
@@ -258,11 +261,12 @@ public function updateItem(Request $request, $id)
     $item->save();
 
     $notificationController = new NotificationController();
+    $changedPrice = $oldPrice != $request->price;
 
     if ($wasOutOfStock && $isInStock) {
         $notificationController->sendItemNotification($item->id);
     }
-    
+
     if ($changedPrice) {
         $notificationController->sendPriceChangeNotification($item->id);
         $notificationController->sendWishlistSaleNotification($item->id);
@@ -286,7 +290,6 @@ public function updateItem(Request $request, $id)
     ], 200);
     
 }
-
     public function createUser(Request $request){
 
         $auth_admin = Auth::guard('admin')->user();
